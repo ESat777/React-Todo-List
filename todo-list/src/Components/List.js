f1161f0b87ec1d6e07dd738bf8b72ae5c77e5b1a
Edis <47144455+ESat777@users.noreply.github.com>
2022-08-28 13:53:33 +0300

    Add local storage

--- todo-list/src/Components/List.js
+++ todo-list/src/Components/List.js
@@ -1,10 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Form from './Form';
 import Todo from './Todo';
 
 function List() {
 
-    const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState(() => {
+		const savedItems = localStorage.getItem("items");	
+		if (savedItems) {	
+		  return JSON.parse(savedItems);	
+		} else {	
+		  return [];	
+		}	
+	  });
+
+    useEffect(() => {
+	    const lsItems = localStorage.getItem("items");
+	    if (!lsItems) localStorage.setItem("items", JSON.stringify(todos));
+	    else setTodos(JSON.parse(lsItems));
+	  }, []);
+
+    useEffect(() => {
+	    localStorage.setItem("items", JSON.stringify(todos));
+	  }, [todos]);
 
     const addTodo = todo =>{
         if(!todo.text || /^\s*$/.test(todo.text)){
